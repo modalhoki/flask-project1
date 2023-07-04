0ab538db3a43341c24e31039daa6237cd235919d
naufal.sunandar <hacker.elite44@gmail.com>
2023-07-04 14:58:19 +0700

    remove unecessary import

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request
 from expertSystem import inference
-from helperFunction import reshape
 import joblib
 import pandas as pd
 
